6536f9a6380bbb0497567e9cc672dcf0d51015c8
jdobie11 dobes <jonathon.dobie@kellogg.northwestern.edu>
2025-01-30 01:00:45 +0000

    lab 2

--- labs/2-conditionals.rb
+++ labs/2-conditionals.rb
@@ -30,3 +30,11 @@ puts die2
 # 3. create a variable for the total and write it to the screen
 total = die1 + die2
 puts "The total is: #{total}"
+
+if total == 7 or total == 11
+    puts "YOU WIN!"
+elsif [2, 3, 12].include?(total)
+    puts "YOU LOSE!"
+else
+    puts "THE POINT IS #{total}"
+end
