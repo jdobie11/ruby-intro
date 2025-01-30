c5b2deab3a8b1dafafb2701d1d39c4964a5788ee
jdobie11 dobes <jonathon.dobie@kellogg.northwestern.edu>
2025-01-30 00:42:56 +0000

    number 1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,19 +1,72 @@
-# To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
-# ruby 1-data.rb
+# # To run this code, be sure your current working directory
+# # is the same as where this file is located and then run:
+# # ruby 1-data.rb
 
-# In Ruby, there are different *types* of data:
+# # In Ruby, there are different *types* of data:
 
-# Numbers
+# # Numbers
 
-# Non-numbers
+# puts 2 + 3 
+# puts 5-4
+# puts 5 * 3 
+# puts 5 / 2
+# puts 5/ 2.0
 
-# True, False
 
-# Nothing
+# # Non-numbers
+# puts "Hello, World!"
 
-# Variables
+# puts "Mondays" + " aren't fun"
 
-# Combine strings and variables
+# puts "friday " * 3
+
+# puts "friday " + 3.to_s
+
+# # True, False
+
+# puts true
+# puts false
+
+
+
+# # Nothing
+
+# puts nil
+
+# # Variables
+
+# monday = 1
+
+# puts monday
+
+
+# # Combine strings and variables
+
+# first_name = "boba"
+# greeting = "Hello, #{first_name}"
+
+# puts greeting
+
+# quantity = 5
+
+# puts "tacos: #{quantity}"
 
 # String manipulation
+
+
+puts "Hello".length
+
+puts "how are you today?".capitalize
+
+puts "how are you today?".reverse
+
+woRds = "how are you"
+
+puts woRds.upcase
+
+puts woRds.downcase
+
+puts woRds.swapcase
+
+puts "\"\""
+
